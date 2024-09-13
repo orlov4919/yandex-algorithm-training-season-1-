e36cfb0b198a628b3e712871d847a67ebe2d567a
orlov4919 <drorlov@stud.etu.ru>
2024-09-13 15:37:15 +0300

    Особые случаи, исправлена задача А на go

--- 1 Особые случаи/Задача А/A.go
+++ 1 Особые случаи/Задача А/A.go
@@ -1,1 +1,20 @@
-package Задача_А
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var tRoom, tWant int
+	var state string
+
+	fmt.Scan(&tRoom, &tWant)
+	fmt.Scan(&state)
+
+	if state == "fan" || tRoom >= tWant && state == "heat" || tRoom <= tWant && state == "freeze" {
+		fmt.Println(tRoom)
+	} else {
+		fmt.Println(tWant)
+	}
+
+}
